d0d55d2cb92289db88758766b21d8fad49ed80ca
Kevin42127 <tyouxipindao@gmail.com>
2025-12-03 08:03:23 +0800

    調整 Android 平台的上邊距使其與 iOS 一致

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './styles/main.css';
 
+const isAndroid = /Android/i.test(navigator.userAgent);
+if (isAndroid) {
+  document.documentElement.classList.add('android');
+}
+
 createApp(App).mount('#app');
 
